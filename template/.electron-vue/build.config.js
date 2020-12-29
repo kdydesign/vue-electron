4e862c7aae6b08912d7189dab870a78e60f160e3
kdydesign <kdydesign@gmail.com>
2020-12-29 12:58:13 +0900

    fix: fixed electron-packager platform build error

--- template/.electron-vue/build.config.js
+++ template/.electron-vue/build.config.js
@@ -11,6 +11,5 @@ module.exports = {
   icon: path.join(__dirname, '../build/icons/icon'),
   ignore: /(^\/(src|test|\.[a-z]+|README|yarn|static|dist\/web))|\.gitkeep/,
   out: path.join(__dirname, '../build'),
-  overwrite: true,
-  platform: process.env.BUILD_TARGET || 'all'
+  overwrite: true
 }
--- template/.electron-vue/build.js
+++ template/.electron-vue/build.js
@@ -105,7 +105,7 @@ function pack (config) {
 {{#if_eq builder 'packager'}}
 function bundleApp () {
   buildConfig.mode = 'production'
-  packager(buildConfig, (err, appPaths) => {
+  packager(buildConfig).then((err, appPaths) => {
     if (err) {
       console.log(`\n${errorLog}${chalk.yellow('`electron-packager`')} says...\n`)
       console.log(err + '\n')
@@ -116,35 +116,35 @@ function bundleApp () {
 }
 
 {{/if_eq}}
-  function web () {
-    del.sync(['dist/web/*', '!.gitkeep'])
-    webConfig.mode = 'production'
-    webpack(webConfig, (err, stats) => {
-      if (err || stats.hasErrors()) console.log(err)
+function web () {
+  del.sync(['dist/web/*', '!.gitkeep'])
+  webConfig.mode = 'production'
+  webpack(webConfig, (err, stats) => {
+    if (err || stats.hasErrors()) console.log(err)
 
-      console.log(stats.toString({
-        chunks: false,
-        colors: true
-      }))
+    console.log(stats.toString({
+      chunks: false,
+      colors: true
+    }))
 
-      process.exit()
+    process.exit()
+  })
+}
+
+function greeting () {
+  const cols = process.stdout.columns
+  let text = ''
+
+  if (cols > 85) text = 'lets-build'
+  else if (cols > 60) text = 'lets-|build'
+  else text = false
+
+  if (text && !isCI) {
+    say(text, {
+      colors: ['yellow'],
+      font: 'simple3d',
+      space: false
     })
-  }
-
-  function greeting () {
-    const cols = process.stdout.columns
-    let text = ''
-
-    if (cols > 85) text = 'lets-build'
-    else if (cols > 60) text = 'lets-|build'
-    else text = false
-
-    if (text && !isCI) {
-      say(text, {
-        colors: ['yellow'],
-        font: 'simple3d',
-        space: false
-      })
-    } else console.log(chalk.yellow.bold('\n  lets-build'))
-    console.log()
-  }
+  } else console.log(chalk.yellow.bold('\n  lets-build'))
+  console.log()
+}
